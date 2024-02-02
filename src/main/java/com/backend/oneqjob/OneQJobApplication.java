b06254a65aa70f201ab91632b747174150228015
mac <mac@donghee.local>
2024-02-03 00:01:21 +0900

    초기세팅

---
+++ src/main/java/com/backend/oneqjob/OneQJobApplication.java
@@ -0,0 +1,14 @@
+package com.backend.oneqjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneQJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneQJobApplication.class, args);
+    }
+
+
+}
